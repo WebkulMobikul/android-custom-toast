7cf173a1f0b9d9b783af40ae4251df884309d4c3
Aastha <aastha.gupta371@webkul.com>
2018-01-08 18:15:04 +0530

    Beta 1.3

---
+++ app/src/main/java/com/webkul/mobikul/webkulcustomtoast/MainActivity.java
@@ -0,0 +1,50 @@
+package com.webkul.mobikul.webkulcustomtoast;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.webkul.mobikul.customtoast.CustomToast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showCustomToast(View view){
+        CustomToast customToast = new CustomToast(this);
+        customToast.setToastPosition(CustomToast.ToastPosition.CENTER);
+        customToast.setToastIcon(getResources().getDrawable(android.R.drawable.btn_star));
+        customToast.setToastIcon(ContextCompat.getDrawable(this, android.R.drawable.btn_star));
+        customToast.setIconPosition(CustomToast.IconPosition.END);
+        customToast.setToastText(R.string.my_dummy_text);
+        customToast.setTextSize(12, TypedValue.COMPLEX_UNIT_DIP);
+        customToast.setToastBackground(CustomToast.ToastBackgroundType.ROUNDED_CORNERS);
+        customToast.setToastBackgroundColor(Color.BLUE);
+        customToast.setToastTextColor(Color.WHITE);
+        customToast.show();
+    }
+
+
+    public void showInstantSuccessToast(View view){
+        CustomToast.getInstantToast(this, CustomToast.InstantToastType.SUCCESS).show();
+    }
+
+    public void showInstantWarningToast(View view){
+        CustomToast.getInstantToast(this, CustomToast.InstantToastType.WARNING).show();
+    }
+
+    public void showInstantErrorToast(View view){
+        CustomToast.getInstantToast(this, CustomToast.InstantToastType.ERROR).show();
+    }
+}
